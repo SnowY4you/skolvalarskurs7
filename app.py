23fb72ac1cf9a0efb5e59d15b3ed3471f22b2442
SnowY <95910378+SnowY4you@users.noreply.github.com>
2024-11-29 18:59:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -249,6 +249,8 @@ if __name__ == '__main__':
     start_port = 8080
     max_port = 8100
     port = find_free_port(start_port, max_port)
+    print(f"Starting server on port {port}")
     app.run_server(debug=False, host='0.0.0.0', port=port)
+
 
 
